8420b054bdc6a30df8709fc5f96f68eeb437f38a
King Richard Guevara <42199746+kingrgdev@users.noreply.github.com>
2023-10-04 19:59:29 +0800

    fixes

--- app/Http/Controllers/Api/APIController.php
+++ app/Http/Controllers/Api/APIController.php
@@ -115,35 +115,98 @@ class APIController extends Controller
     }
 
     public function getGameListing(Request $request) {
-        $input = $request->all();
-        $input['start_date_before'] = "2024-02-11T16:35:00-05:00";
-        $gameData = $this->games($input);
+        // $input = $request->all();
+        // $input['start_date_before'] = "2024-04-11T16:35:00-05:00";
+
+        // $gameData = $this->games($input);
+
+        // $game_array = [];
+
+        // if ($gameData['status'] && !empty($gameData['data'])) {
+        //     $response = $gameData['data'];
+        //     $count = 0;
+
+        //     foreach ($response['data'] as $game) {
+        //         $count++;
+
+        //             // SportsBook
+        //             $sports_book_input = ['game_id' => $game['id']];
+        //             $sports_book = $this->sportsBook($sports_book_input);
+        //             $sports_book_data = $sports_book[0]['data'];
+
+        //             $home_team = $game['home_team_info'];
+        //             $away_team = $game['away_team_info'];
+
+        //         if ( isset($home_team) && isset($away_team) ) {
+
+        //             $upcomingGameOddsInput = [];
+        //             $upcomingGameOddsInput['game_id']    =   $game['id'];
+        //             $upcomingGameOddsInput['market_name']    =   "Moneyline";
+        //             // $upcomingGameOddsInput['is_main']    =   true;
+        //             $upcomingGameOddsInput['team_id']    =   $home_team['id'];
+        //             // Retrieve odds per team
+        //             // Home Team
+        //             $upcomingGameOddsHomeTeam = $this->upcomingGameOdds($upcomingGameOddsInput);
+
+        //             // Away Team
+        //             $upcomingGameOddsInput['team_id']    =   $away_team['id'];
+        //             $upcomingGameOddsAwayTeam = $this->upcomingGameOdds($upcomingGameOddsInput);
+
+        //             array_push($game_array, [
+        //                 'game' => $game,
+        //                 'sports_book' => $sports_book_data,
+        //                 'home_team' =>  $upcomingGameOddsHomeTeam['data'][0]['odds'],
+        //                 'away_team' => $upcomingGameOddsAwayTeam['data'][0]['odds']
+        //             ]);
+
+        //             dd($game_array);
+
+        //         }
+                    
+        //     }
+        // }
+
+
+        $sports = [
+            'football','basketball','baseball','mma','boxing','hockey','soccer','tennis','golf','motorsports','esports','wrestling','aussie-rules','rugby'
+        ];
+
+        $input = [];
+        $input['start_date_before'] = "2024-04-11T16:35:00-05:00";
+        $input['sports']    =   $sports;
+
+        $league_raw = $this->leagues($input);
+        $league_data = $league_raw[0]['data'];
+
+        $input['leagues']    =   $league_data;
 
         $game_array = [];
+        $gameData = $this->games($input);
 
         if ($gameData['status'] && !empty($gameData['data'])) {
             $response = $gameData['data'];
             $count = 0;
-
+            
             foreach ($response['data'] as $game) {
                 $count++;
 
-                if ($count == 2) {
                     // SportsBook
-                    $sports_book_input = ['game_id' => $game['id']];
-                    $sports_book = $this->sportsBook($sports_book_input);
-                    $sports_book_data = $sports_book[0]['data'];
+                    // $sports_book_input = ['game_id' => $game['id']];
+                    // $sports_book = $this->sportsBook($sports_book_input);
+                    // $sports_book_data = $sports_book[0]['data'];
+
+                    // SportsBook
+                    $sports_book_data = $this->defaultSporksBook();
 
                     $home_team = $game['home_team_info'];
                     $away_team = $game['away_team_info'];
 
+                if ( isset($home_team) && isset($away_team) ) {
+
                     $upcomingGameOddsInput = [];
                     $upcomingGameOddsInput['game_id']    =   $game['id'];
-                    $upcomingGameOddsInput['market_name']    =   "Moneyline";
-                    // $upcomingGameOddsInput['is_main']    =   true;
                     $upcomingGameOddsInput['team_id']    =   $home_team['id'];
-
-
+                    
                     // Retrieve odds per team
                     // Home Team
                     $upcomingGameOddsHomeTeam = $this->upcomingGameOdds($upcomingGameOddsInput);
@@ -152,18 +215,34 @@ class APIController extends Controller
                     $upcomingGameOddsInput['team_id']    =   $away_team['id'];
                     $upcomingGameOddsAwayTeam = $this->upcomingGameOdds($upcomingGameOddsInput);
 
+                    $market_input = [];
+                    $market_input['game_id'] = $game['id'];
+                    $market_input['leagues'] =   ['WTA'];
+                    $market_input['sports']  =   ['tennis'];
+                    $market_query = $this->markets($market_input);
+                    $market_categories_query = $this->marketCategories($market_input);
+
                     array_push($game_array, [
                         'game' => $game,
-                        'sports_book' => $sports_book_data,
-                        'home_team' =>  $upcomingGameOddsHomeTeam['data'][0]['odds'],
-                        'away_team' => $upcomingGameOddsAwayTeam['data'][0]['odds']
+                        // 'sports_book' => $sports_book_data,
+                        'home_team_odds' =>  $upcomingGameOddsHomeTeam['data'][0]['odds'],
+                        'away_team_odds' => $upcomingGameOddsAwayTeam['data'][0]['odds'],
+                        // 'market'  => $market_query[0]['data'],
+                        // 'market_categories' =>  $market_categories_query[0]['data'],
                     ]);
 
                     dd($game_array);
-                    
+
                 }
+                    
             }
+
         }
+
+
+
+
+        
     }
 
 }
--- app/Http/Traits/OddsJamAPITrait.php
+++ app/Http/Traits/OddsJamAPITrait.php
@@ -19,8 +19,42 @@ trait OddsJamAPITrait
                 "Content-Type: application/json"
             );
 
-            $url =  "https://api-external.oddsjam.com/api/v2/games/?include_team_info=true&key=" . config('services.oddsjam.key') . ( $data['start_date_before'] ? '&start_date_before=' .$data['start_date_before'] : '' );
-            
+            $sports ='';
+            if(!empty($data)) {
+                foreach ($data['sports'] as $sport ) {
+                    $sports .= '&sport=' . $sport;
+                }
+            }
+
+            $leagues ='';
+            if(!empty($data)) {
+                foreach ($data['leagues'] as $league ) {
+                    $leagues .= '&league=' . $league;
+                }
+            }
+
+            // $url =  "https://api-external.oddsjam.com/api/v2/games/?include_team_info=true&key=" . config('services.oddsjam.key') . ( $data['start_date_before'] ? '&start_date_before=' .$data['start_date_before'] : '' )  . $leagues;
+
+            $baseURL = "https://api-external.oddsjam.com/api/v2/games/";
+
+            $queryParams = [
+                'include_team_info' => 'true',
+                'key' => config('services.oddsjam.key')
+            ];
+
+            if (isset($data['start_date_before']) && $data['start_date_before']) {
+                $queryParams['start_date_before'] = $data['start_date_before'];
+            }
+
+            // Assuming $leagues is something like "&league=soccer&league=football"
+            parse_str(ltrim($leagues, '&'), $leaguesArray);
+
+            // Merge the two arrays
+            $queryParams = array_merge($queryParams, $leaguesArray);
+
+            // Using Laravel's http_build_query function
+            $url = $baseURL . '?' . http_build_query($queryParams);
+
             curl_setopt($curl, CURLOPT_URL, $url);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($curl, CURLOPT_POST, false);
@@ -73,8 +107,15 @@ trait OddsJamAPITrait
             $headers = array(
                 "Content-Type: application/json"
             );
+            
+            $sports ='';
+            if(!empty($data)) {
+                foreach ($data['sports'] as $sport ) {
+                    $sports .= '&sport=' . $sport;
+                }
+            }
 
-            curl_setopt($curl, CURLOPT_URL, "https://api-external.oddsjam.com/api/v2/leagues/?key=" . config('services.oddsjam.key'));
+            curl_setopt($curl, CURLOPT_URL, "https://api-external.oddsjam.com/api/v2/leagues/?key=" . config('services.oddsjam.key') . $sports );
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($curl, CURLOPT_POST, false);
             curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
@@ -127,7 +168,7 @@ trait OddsJamAPITrait
                 "Content-Type: application/json"
             );
 
-            curl_setopt($curl, CURLOPT_URL, "https://api-external.oddsjam.com/api/v2/game-odds?key=" . config('services.oddsjam.key') . "&game_id=" . $data['game_id'] . "&team_id=" . $data['team_id'] . "&market_name=" . $data['market_name'] );
+            curl_setopt($curl, CURLOPT_URL, "https://api-external.oddsjam.com/api/v2/game-odds?key=" . config('services.oddsjam.key') . "&game_id=" . $data['game_id'] . "&team_id=" . $data['team_id']);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($curl, CURLOPT_POST, false);
             curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
@@ -547,7 +588,37 @@ trait OddsJamAPITrait
                 "Content-Type: application/json"
             );
 
-            curl_setopt($curl, CURLOPT_URL, "https://api-external.oddsjam.com/api/v2/market-categories?key=" . config('services.oddsjam.key') . "&team_id=18F74A0DDBD1&sports=mma&label=Moneyline");
+            $sports ='';
+            if(!empty($data)) {
+                foreach ($data['sports'] as $sport ) {
+                    $sports .= '&sport=' . $sport;
+                }
+            }
+
+            $leagues ='';
+            if(!empty($data)) {
+                foreach ($data['leagues'] as $league ) {
+                    $leagues .= '&league=' . $league;
+                }
+            }
+
+            $baseURL = "https://api-external.oddsjam.com/api/v2/market-categories/";
+
+            $queryParams = [
+                'include_team_info' => 'true',
+                'key' => config('services.oddsjam.key')
+            ];
+
+            // Assuming $leagues is something like "&league=soccer&league=football"
+            parse_str(ltrim($leagues, '&'), $leaguesArray);
+
+            // Merge the two arrays
+            $queryParams = array_merge($queryParams, $leaguesArray);
+
+            // Using Laravel's http_build_query function
+            $url = $baseURL . '?' . http_build_query($queryParams);
+
+            curl_setopt($curl, CURLOPT_URL, $url);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($curl, CURLOPT_POST, false);
             curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
@@ -600,7 +671,43 @@ trait OddsJamAPITrait
                 "Content-Type: application/json"
             );
 
-            curl_setopt($curl, CURLOPT_URL, "https://api-external.oddsjam.com/api/v2/markets?key=" . config('services.oddsjam.key') ."&game_id=" . $data['game_id']);
+
+            $sports ='';
+            if(!empty($data)) {
+                foreach ($data['sports'] as $sport ) {
+                    $sports .= '&sport=' . $sport;
+                }
+            }
+
+            $leagues ='';
+            if(!empty($data)) {
+                foreach ($data['leagues'] as $league ) {
+                    $leagues .= '&league=' . $league;
+                }
+            }
+
+            $baseURL = "https://api-external.oddsjam.com/api/v2/markets/";
+
+            $queryParams = [
+                'include_team_info' => 'true',
+                'key' => config('services.oddsjam.key')
+            ];
+
+            if (isset($data['start_date_before']) && $data['start_date_before']) {
+                $queryParams['start_date_before'] = $data['start_date_before'];
+            }
+
+            // Assuming $leagues is something like "&league=soccer&league=football"
+            parse_str(ltrim($leagues, '&'), $leaguesArray);
+
+            // Merge the two arrays
+            $queryParams = array_merge($queryParams, $leaguesArray);
+
+            // Using Laravel's http_build_query function
+            $url = $baseURL . '?' . http_build_query($queryParams);
+
+
+            curl_setopt($curl, CURLOPT_URL, $url );
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($curl, CURLOPT_POST, false);
             curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
@@ -747,5 +854,51 @@ trait OddsJamAPITrait
 
         return $output;
 
+    }
+
+    public function defaultSporksBook() {
+        $data = [
+                '10bet',
+                '888sport',
+                'bet365',
+                'BET99',
+                'Betano',
+                'Betcris',
+                'BetDSI',
+                'BetNow',
+                'BetOnline',
+                'BetonUSA',
+                'Betsafe',
+                'Betsson',
+                'BetUS',
+                'BetVictor',
+                'Betway',
+                'BookMaker',
+                'bwin',
+                'Casumo',
+                'ComeOn!',
+                'Coolbet',
+                'Everygame',
+                'Jazz Sports',
+                'LeoVegas',
+                'Looselines',
+                'LowVig',
+                'Mise-o-jeu',
+                'MyBookie',
+                'Nitrogen',
+                'partypoker',
+                'Pinnacle',
+                'PowerPlay',
+                'Royal Panda',
+                'Sportsbetting.ag',
+                'Sports Interaction',
+                'Stake',
+                'Suprabets',
+                'TonyBet',
+                'William Hill',
+                'Xbet'
+            ];
+
+        return $data;
     }
 }
