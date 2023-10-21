ffb100223e66acb1a3434a707a57dba044e5656a
King Richard Guevara <42199746+kingrgdev@users.noreply.github.com>
2023-10-22 00:26:27 +0800

    odds game table

---
+++ app/Services/GameOdds/GameOdds.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Services\GameOdds;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+/**
+ * Class GameOdds
+ * @package App\Services\GameOdds
+ * @author Richard Guevara
+ */
+
+class GameOdds extends Model
+{
+    use SoftDeletes, LogsActivity;
+
+    protected $table = 'gameodds';
+
+    protected static $logAttributes = [ 
+        'bet_name',
+        'bet_points',
+        'bet_price',
+        'bet_type',
+        'game_id',
+        'uid',
+        'is_live',
+        'is_main',
+        'league',
+        'player_id',
+        'selection',
+        'selection_line',
+        'selection_points',
+        'sport',
+        'sportsbook',
+        'timestamp',
+        'entry_id',
+        'type',
+        'is_active'
+    ];
+
+    protected static $logName = 'gameodds';
+
+    public function getDescriptionForEvent(string $eventName): string
+    {
+        return "GameOdds has been {$eventName}";
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'bet_name',
+        'bet_points',
+        'bet_price',
+        'bet_type',
+        'game_id',
+        'uid',
+        'is_live',
+        'is_main',
+        'league',
+        'player_id',
+        'selection',
+        'selection_line',
+        'selection_points',
+        'sport',
+        'sportsbook',
+        'timestamp',
+        'entry_id',
+        'type',
+        'is_active'
+    ];
+}
---
+++ database/migrations/2023_10_21_161219_create_gameodds_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGameOddsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gameodds', function (Blueprint $table) {
+            
+            $table->bigIncrements('id');
+            $table->longtext('bet_name')->nullable();
+            $table->decimal('bet_points', 8,2 )->default(0.00);
+            $table->integer('bet_price')->default(0.00);
+            $table->string('bet_type')->nullable();
+            $table->string('game_id')->nullable();
+            $table->string('uid')->nullable();
+            $table->string('is_live')->default( false );
+            $table->string('is_main')->default( false );
+            $table->string('league')->nullable();
+            $table->string('player_id')->nullable();
+            $table->string('selection')->nullable();
+            $table->string('selection_line')->nullable();
+            $table->decimal('selection_points')->default(0.00);
+            $table->longtext('sport')->nullable();
+            $table->longtext('sportsbook')->nullable();
+            $table->string('timestamp')->nullable();
+            $table->string('entry_id')->nullable();
+            $table->string('type')->nullable();
+
+            $table->tinyInteger('is_active')->default(1);
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gameodds');
+    }
+}
