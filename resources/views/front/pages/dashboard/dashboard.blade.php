ee1cda7d6b03226df8dedd57860378826ece7f39
King Richard Guevara <42199746+kingrgdev@users.noreply.github.com>
2023-11-03 23:10:08 +0800

    fixes

--- resources/views/front/pages/dashboard/dashboard.blade.php
+++ resources/views/front/pages/dashboard/dashboard.blade.php
@@ -190,7 +190,7 @@
                         <div class="flex min-w-0 flex-1 items-center justify-center gap-1.5">
                             <div class="h-4 w-4"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true"><path fill-rule="evenodd" d="M4.755 10.059a7.5 7.5 0 0112.548-3.364l1.903 1.903h-3.183a.75.75 0 100 1.5h4.992a.75.75 0 00.75-.75V4.356a.75.75 0 00-1.5 0v3.18l-1.9-1.9A9 9 0 003.306 9.67a.75.75 0 101.45.388zm15.408 3.352a.75.75 0 00-.919.53 7.5 7.5 0 01-12.548 3.364l-1.902-1.903h3.183a.75.75 0 000-1.5H2.984a.75.75 0 00-.75.75v4.992a.75.75 0 001.5 0v-3.18l1.9 1.9a9 9 0 0015.059-4.035.75.75 0 00-.53-.918z" clip-rule="evenodd"></path></svg>
                             </div>
-                            <div class="min-w-0 overflow-hidden text-ellipsis whitespace-nowrap">Refresh</div>
+                            <a class="min-w-0 overflow-hidden text-ellipsis whitespace-nowrap" href="{{url('/dashboard')}}">Refresh</a>
                         </div>
                     </button>
                     <div role="button" tabindex="-1" class="">
@@ -658,6 +658,43 @@
                     popup.classList.add('hidden');
                 }
             });
+        });
+
+
+        let source;
+
+        $("#playButton").on('click', function() {
+            let buttonText = $(this).text();
+
+            if(buttonText == "Play") {
+                // Change the button text to Stop
+                $(this).text('Stop');
+                
+                // Start streaming
+                if(typeof(EventSource) !== "undefined") {
+                    source = new EventSource("/api/odds-push-streams");
+
+                    source.onmessage = function(event) {
+                        // Handle the incoming data here
+                        console.log(event.data);
+                    };
+                    
+                    source.onerror = function(event) {
+                        console.error("EventSource failed:", event);
+                        source.close();
+                    };
+                } else {
+                    alert("Sorry! Your browser doesn't support server-sent events.");
+                }
+            } else {
+                // Change the button text to Play
+                $(this).text('Play');
+
+                // Stop streaming
+                if(source) {
+                    source.close();
+                }
+            }
         });
 
 
