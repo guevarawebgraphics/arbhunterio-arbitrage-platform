fd144a22382aee2b5fd0b9b04715e0b4ff4a75fb
Bryan James Dela Luya <delaluya@gmail.com>
2023-11-03 20:00:15 +0800

    add indexing

---
+++ database/migrations/2023_11_03_115225_alter_gameodds_add_indexing.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterGameoddsAddIndexing extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('gameodds', function (Blueprint $table) {
+            $table->index('bet_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('gameodds', function (Blueprint $table) {
+            $table->dropIndex('bet_name');
+        });
+    }
+}
